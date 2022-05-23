dde65af31ed499e47d0ebaa0a9b1abe3198ae73a
Narmeenm <narmeenmassalha4975@gmail.com>
2022-05-24 00:38:26 +0300

    pandas table

--- main.py
+++ main.py
@@ -16,7 +16,11 @@ def index():
     whr_2017 = pd.read_csv("./archive/2017.csv")
     whr_2016 = pd.read_csv("./archive/2016.csv")
     whr_2015 = pd.read_csv("./archive/2015.csv")
-    return render_template('landing.html')
+    whr_2017 = whr_2017.rename(columns= {'Family':'Social support', 'Happiness.Rank':'Happiness Rank', 'Happiness.Score':'Happiness Score','Economy..GDP.per.Capita.':'Economy (GDP per Capita)', 'Health..Life.Expectancy.':'Health (Life Expectancy)', 'Trust..Government.Corruption.':'Trust (Government Corruption)', 'Dystopia.Residual':'Dystopia Residual'})
+    whr_2018 = whr_2018.rename(columns = {'Overall rank':'Happiness Rank','Country or region':'Country', 'GDP per capita':'Economy (GDP per Capita)', 'Freedom to make life choices':'Freedom', 'Healthy life expectancy':'Health (Life Expectancy)', 'Perceptions of corruption':'Trust (Government Corruption)'})
+    whr_2019 = whr_2019.rename(columns = {'Overall rank':'Happiness Rank','Score':'Happiness Score','Country or region':'Country', 'GDP per capita':'Economy (GDP per Capita)', 'Freedom to make life choices':'Freedom', 'Healthy life expectancy':'Health (Life Expectancy)', 'Perceptions of corruption':'Trust (Government Corruption)'})
+    table_2015 = whr_2015.head(10).reset_index(drop=True)
+    return render_template('landing.html',table=table_2015.to_html())
 
 
 
